22a28dee1f2f28e1f058cc45b5b947d8fbc3489e
hsm <hll2071@naver.com>
2025-12-14 20:58:25 +0900

    feat(comment): CommentResponse 구현

---
+++ src/main/java/com/example/gcsuhang/comment/service/response/CommentResponse.java
@@ -0,0 +1,39 @@
+package com.example.gcsuhang.comment.service.response;
+
+import com.example.gcsuhang.comment.entity.Comment;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CommentResponse {
+    private final Long id;
+    private final String content;
+    private final Long articleId;
+    private final Long writerId;
+    private final String path;
+    private final Boolean deleted;
+    private final LocalDateTime createdAt;
+
+    private CommentResponse(Long id, String content, Long articleId, Long writerId, String path, Boolean deleted,
+            LocalDateTime createdAt) {
+        this.id = id;
+        this.content = content;
+        this.articleId = articleId;
+        this.writerId = writerId;
+        this.path = path;
+        this.deleted = deleted;
+        this.createdAt = createdAt;
+    }
+
+    public static CommentResponse from(Comment comment) {
+        return new CommentResponse(
+                comment.getId(),
+                comment.getContent(),
+                comment.getArticleId(),
+                comment.getWriterId(),
+                comment.getPath(),
+                comment.getDeleted(),
+                comment.getCreatedAt());
+    }
+}
